1d76ff89a1c3fee211cdd7edc92622df839c2f95
Paige Tuttosi <ptuttosi@rcg-visnode-01.rcg.sfu.ca>
2019-10-18 09:32:55 -0700

    Change to pandas library

--- mockDatabase.py
+++ mockDatabase.py
@@ -1,14 +1,29 @@
 import csv
-textfile = open("mockData.sql", "w+")
+import pandas as pd
 
-createTableTemplate = open("createTableTemplate.sql").read()
-createTableTemplate.format(tablename = "subject", columns)
-with open("ClinicalInfo_final.csv") as csvfile:
-	columns = csvfile.readline()
-	for line in csv.reader(csvfile, delimiter = ","):
-		pass
+#textfile = open("mockData.sql", "w+")
 
-textfile.close
+#createTableTemplate = open("createTableTemplate.sql").read()
+#createTableTemplate.format(tablename = "subject", columns)
+
+#read in the daata and save the headers to a list
+data = pd.read_csv("ClinicalInfo_final.csv")
+colnames = list(data)
+
+#check that RID exists or throw an error
+if("RID" not in colnames):
+	raise Error("Incorrect data format, please standardize column nammes and try again")
+
+"""
+	check subject level time stamp
+	extract the rows of data which do not change for unique RID
+	These belong to the subjects table
+"""
+	
+data_uniqueRID = data.assign(RID==data.RID.unique)
+print(data_uniqueRID)
+
+csvfile.close
 
 
 
