4f6505beff87730f8083f6346296fb028c78589f
Paige T <ptuttosi@sfu.ca>
2019-10-19 13:35:44 -0700

    Create hash table
    Hash table has keys related to the levels of the SQL tables
    Next we will check for changes at each level

--- mockDatabase.py
+++ mockDatabase.py
@@ -8,25 +8,57 @@ import pandas as pd
 
 #read in the daata and save the headers to a list
 data = pd.read_csv("ClinicalInfo_final.csv")
+
 colnames = list(data)
 
 #check that RID exists or throw an error
 if("RID" not in colnames):
-	raise Error("Incorrect data format, please standardize column nammes and try again")
+    raise Error("Incorrect data format, please standardize column nammes and try again")
 
 """
-	check subject level time stamp
-	extract the rows of data which do not change for unique RID
-	These belong to the subjects table
+    check subject level time stamp
+    extract the rows of data which do not change for unique RID
+    These belong to the subjects table
 """
-	
-data_uniqueRID = data.assign(RID==data.RID.unique)
-print(data_uniqueRID)
 
-csvfile.close
+data_uniqueRID = data.RID.unique()
+subjectVariables = []
+visitsVariables = []
+subjects = {}
+for RID in data_uniqueRID:
+    subjects[RID] = {}
+for key in list(subjects.keys())[0:5]:
+    print(key, subjects[key])
+for RID in subjects:
+  row = data.loc[data['RID'] == RID]
+  visit = row["VISCODE"]
+  new = row.drop(["RID", "VISCODE"], axis = 1)
+  for i, viscode in enumerate(visit):
+    subjects[RID][viscode] = new.iloc[i]
 
-
-
-
-
-
+print(subjects)
+exit(0)
+#for key in subjects:
+#    for varible in subjects[key]:
+#        if all(elem == variable[0] for elem in variable)
+#            subjectVariable.append()
+#
+#myDict = {
+#    1: {
+#        'v1': {
+#            "gender": ["male"],
+#            "race": ["latino"],
+#            "age": [57, 58]
+#        },
+#        'v2': [[], []],
+#        'v3': [[]],
+#    }
+#}
+#
+#myArray = [1,2]
+#
+#if 3 not in myArray:
+#    myArray.append(3)
+#
+#
+#myDict[1]['v1'][0]['gender'] == myDict[1]['v2'][0]['gender']
