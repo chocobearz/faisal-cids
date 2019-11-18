1af4f6fe34cda0393c0e3ef2fee4a0dbf7216917
Paige T <ptuttosi@sfu.ca>
2019-11-18 14:49:20 -0800

    Update dictionary to hold arrays of arrays
    For multiple measures on the same day the dictionary needs to
    have an array of arrays

--- mockDatabase.py
+++ mockDatabase.py
@@ -13,12 +13,12 @@ colnames = list(data)
 
 #check that RID exists or throw an error
 if("RID" not in colnames):
-    raise Error("Incorrect data format, please standardize column nammes and try again")
+  raise Error("Incorrect data format, please standardize column nammes and try again")
 
 """
-    check subject level time stamp
-    extract the rows of data which do not change for unique RID
-    These belong to the subjects table
+  check subject level time stamp
+  extract the rows of data which do not change for unique RID
+  These belong to the subjects table
 """
 
 data_uniqueRID = data.RID.unique()
@@ -27,38 +27,37 @@ visitsVariables = []
 subjects = {}
 for RID in data_uniqueRID:
     subjects[RID] = {}
-for key in list(subjects.keys())[0:5]:
-    print(key, subjects[key])
 for RID in subjects:
   row = data.loc[data['RID'] == RID]
   visit = row["VISCODE"]
   new = row.drop(["RID", "VISCODE"], axis = 1)
   for i, viscode in enumerate(visit):
-    subjects[RID][viscode] = new.iloc[i]
+    if viscode in subjects[RID]:
+      subjects[RID][viscode].append(dict(new.iloc[i]))
+    else:
+      subjects[RID][viscode] = [dict(new.iloc[i])]
 
-print(subjects)
+all_rid_same = {}
+
+for key in subjects:
+  all_rid_same[key] = {}
+  for variable in subjects[key]:
+    for column_name in subjects[key][variable]:
+      entry_value = subjects[key][variable]
+      if column_name not in all_rid_same[key]:
+        all_rid_same[key][column_name] = [entry_value]
+      else:
+        all_rid_same[key][column_name].append(entry_value)
+
 exit(0)
-#for key in subjects:
-#    for varible in subjects[key]:
-#        if all(elem == variable[0] for elem in variable)
-#            subjectVariable.append()
-#
-#myDict = {
-#    1: {
-#        'v1': {
-#            "gender": ["male"],
-#            "race": ["latino"],
-#            "age": [57, 58]
-#        },
-#        'v2': [[], []],
-#        'v3': [[]],
-#    }
-#}
-#
-#myArray = [1,2]
-#
-#if 3 not in myArray:
-#    myArray.append(3)
-#
-#
-#myDict[1]['v1'][0]['gender'] == myDict[1]['v2'][0]['gender']
+for key in all_rid_same:
+  for column_name in all_rid_same[key]:
+    print(all_rid_same[key][column_name])
+    print(
+      all(
+        value == all_rid_same[key][column_name][0]
+          for value in all_rid_same[key][column_name]
+      )
+    )
+
+#print(subjectVariables)
