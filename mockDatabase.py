ab48a85d88ee518c8f4be5540966859ea3547498
Paige T <ptuttosi@sfu.ca>
2020-01-05 09:54:40 -0800

    Update comments

--- mockDatabase.py
+++ mockDatabase.py
@@ -3,12 +3,9 @@ import pandas as pd
 import yaml
 
 with open(r"config.yaml") as config: ##is the "r" a typo?
-    # The FullLoader parameter handles the conversion from YAML
-    # scalar values to Python the dictionary format
-    vars_list = yaml.load(config, Loader=yaml.FullLoader)
-
-    print(vars_list)
-    exit(0)
+  # The FullLoader parameter handles the conversion from YAML
+  # scalar values to Python the dictionary format
+  vars_list = yaml.load(config, Loader=yaml.FullLoader)
 
 #read in the data and save the headers to a list
 with pd.read_csv("ClinicalInfo_final.csv") as data:
@@ -28,9 +25,10 @@ with pd.read_csv("ClinicalInfo_final.csv") as data:
     raise Error("Incorrect data format, please standardize column nammes and try again")
 
   """
-    check subject level time stamp
-    extract the rows of data which do not change for unique RID
-    These belong to the subject table
+  Create a dictionary with three levels of keys
+  Subject key: RID
+  Visit key: VISCODE
+  Repeat key: REPEATCODE
   """
 
   data_uniqueRID = data.RID.unique()
@@ -52,72 +50,80 @@ with pd.read_csv("ClinicalInfo_final.csv") as data:
       for i, repeatcode in enumerate(repeat):
         subject[RID][viscode][repeatcode] = [dict(new.iloc[i])]
 
+"""
+  check subject level time stamp
+  extract the columns of data which do not change for unique RID
+  these belong to the subject table
+"""
 
-  all_rid_same = {}
+all_rid_same = {}
+for key in subject:
+  all_rid_same[key] = {}
+  for viscode in subject[key]:
+    for repeatcode in subject[key][viscode]:
+      for idx, _ in enumerate(subject[key][viscode][repeatcode]):
+        for column_name in subject[key][viscode][repeatcode][idx]:
+          entry_value = subject[key][viscode][repeatcode][idx][column_name]
+          if column_name not in all_rid_same[key]:
+            all_rid_same[key][column_name] = [entry_value]
+          else:
+            all_rid_same[key][column_name].append(entry_value)
 
-  for key in subject:
-    all_rid_same[key] = {}
-    for viscode in subject[key]:
-      for repeatcode in subject[key][viscode]:
-        for idx, _ in enumerate(subject[key][viscode][repeatcode]):
-          for column_name in subject[key][viscode][repeatcode][idx]:
-            entry_value = subject[key][viscode][repeatcode][idx][column_name]
-            if column_name not in all_rid_same[key]:
-              all_rid_same[key][column_name] = [entry_value]
-            else:
-              all_rid_same[key][column_name].append(entry_value)
+column_subject_eval = {}
 
-  column_subject_eval = {}
-
-  for key in all_rid_same:
-    for column_name in all_rid_same[key]:
-      subject_bool = all(
-        value == all_rid_same[key][column_name][0]
-        for value in all_rid_same[key][column_name]
-      )
-      if column_name not in column_subject_eval:
-        column_subject_eval[column_name] = [subject_bool]
-      else:
-        column_subject_eval[column_name].append(subject_bool)
-
-  for column_name in column_subject_eval:
-    if (all(value for value in column_subject_eval[column_name])):
-      subjectVariables.append(column_name)
-
-
-  column_visit_eval = {}
-
-
-  for key in subject:
-    for viscode in subject[key]:
-      if (len(subject[key][viscode]) > 1):
-        for column_name in subject[key][viscode][repeatcode][0]:
-          store_values = []
-          for repeatcode in subject[key][viscode]:
-            for visit_dict in subject[key][viscode][repeatcode]:
-              store_values.append(visit_dict[column_name])
-              visit_bool = all(
-              value == store_values[0]
-              for value in store_values
-              )
-            if column_name not in column_visit_eval:
-              column_visit_eval[column_name] = [visit_bool]
-            else:
-              column_visit_eval[column_name].append(visit_bool)
-
-  for column_name in column_visit_eval:
-    if (all(value for value in column_visit_eval[column_name])):
-      if(column_name not in subjectVariables):
-        visitVariables.append(column_name)
+for key in all_rid_same:
+  for column_name in all_rid_same[key]:
+    subject_bool = all(
+      value == all_rid_same[key][column_name][0]
+      for value in all_rid_same[key][column_name]
+    )
+    if column_name not in column_subject_eval:
+      column_subject_eval[column_name] = [subject_bool]
     else:
-      repeatVariables.append(column_name)
+      column_subject_eval[column_name].append(subject_bool)
+for column_name in column_subject_eval:
+  if (all(value for value in column_subject_eval[column_name])):
+    subjectVariables.append(column_name)
 
-#with open("mockData.sql", "w+") as textfile:
+  """
+    check visit level time stamp
+    extract columns of data which do not change for unique viscode of unique ID
+    these belong to the visits table
+    all remaining measures belong to the repeat table
+  """
+
+column_visit_eval = {}
+
+
+for key in subject:
+  for viscode in subject[key]:
+    if (len(subject[key][viscode]) > 1):
+      for column_name in subject[key][viscode][repeatcode][0]:
+        store_values = []
+        for repeatcode in subject[key][viscode]:
+          for visit_dict in subject[key][viscode][repeatcode]:
+            store_values.append(visit_dict[column_name])
+            visit_bool = all(
+            value == store_values[0]
+            for value in store_values
+            )
+          if column_name not in column_visit_eval:
+            column_visit_eval[column_name] = [visit_bool]
+          else:
+            column_visit_eval[column_name].append(visit_bool)
+for column_name in column_visit_eval:
+  if (all(value for value in column_visit_eval[column_name])):
+    if(column_name not in subjectVariables):
+      visitVariables.append(column_name)
+  else:
+    repeatVariables.append(column_name)
+
+with open("mockData.sql", "w+") as textfile:
   
-  #createTableTemplate = open("createTableTemplate.sql").read()
+  updateTableTemplate = open("createTableTemplate.sql").r
+
+  updateTableTemplate.format(tablename = "subject", col
+
+  updateTableTemplate.format(tablename = "visit", col
   
-  #createTableTemplate.format(tablename = "subject", columns)
-  
-  #createTableTemplate.format(tablename = "visit", columns)
-  
-  #createTableTemplate.format(tablename = "repeat", columns)
+  updateTableTemplate.format(tablename = "repeat", columns)
