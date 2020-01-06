b3d70f9c9d49790fbbaec89b65edfc37b8dd0465
Paige T <ptuttosi@sfu.ca>
2020-01-05 20:56:22 -0800

    remove with for pandas load
    Fix indentation as well

--- mockDatabase.py
+++ mockDatabase.py
@@ -1,54 +1,65 @@
 import csv
 import pandas as pd
 import yaml
+import argparse
+
+#parser = argparse.ArgumentParser(description='Process some integers.')
+#parser.add_argument('integers', metavar='N', type=int, nargs='+',
+#                    help='an integer for the accumulator')
+#parser.add_argument('--sum', dest='accumulate', action='store_const',
+#                    const=sum, default=max,
+#                    help='sum the integers (default: find the max)')
+
+# args = parser.parse_args()
+# print(args.accumulate(args.integers))
 
 with open(r"config.yaml") as config: ##is the "r" a typo?
   # The FullLoader parameter handles the conversion from YAML
   # scalar values to Python the dictionary format
   vars_list = yaml.load(config, Loader=yaml.FullLoader)
 
+
 #read in the data and save the headers to a list
-with pd.read_csv("ClinicalInfo_final.csv") as data:
-  
-  colnames = list(data)
+data = pd.read_csv("ClinicalInfo_final.csv")
 
-  #check that RID exists or throw an error
-  if("RID" not in colnames):
-    raise Error("Incorrect data format, please standardize column nammes and try again")
+colnames = list(data)
 
-  #check that RID exists or throw an error
-  if("VISCODE" not in colnames):
-    raise Error("Incorrect data format, please standardize column nammes and try again")
+#check that RID exists or throw an error
+if("RID" not in colnames):
+  raise Error("Incorrect data format, please standardize column nammes and try again")
 
-  #check that RID exists or throw an error
-  if("REPEATCODE" not in colnames):
-    raise Error("Incorrect data format, please standardize column nammes and try again")
+#check that RID exists or throw an error
+if("VISCODE" not in colnames):
+  raise Error("Incorrect data format, please standardize column nammes and try again")
 
-  """
-  Create a dictionary with three levels of keys
-  Subject key: RID
-  Visit key: VISCODE
-  Repeat key: REPEATCODE
-  """
+#check that RID exists or throw an error
+if("REPEATCODE" not in colnames):
+  raise Error("Incorrect data format, please standardize column nammes and try again")
 
-  data_uniqueRID = data.RID.unique()
-  subjectVariables = []
-  visitVariables = []
-  repeatVariables = []
+"""
+Create a dictionary with three levels of keys
+Subject key: RID
+Visit key: VISCODE
+Repeat key: REPEATCODE
+"""
 
-  subject = {}
-  for RID in data_uniqueRID:
-    subject[RID] = {}
-    row = data.loc[data['RID'] == RID]
-    visit = row["VISCODE"]
-    for viscode in visit:
-      if viscode not in subject[RID]:
-        subject[RID][viscode] = {}
-      vrow = row.loc[row['VISCODE'] == viscode]
-      repeat = vrow["REPEATCODE"]
-      new = vrow.drop(["RID", "VISCODE", "REPEATCODE"], axis = 1)
-      for i, repeatcode in enumerate(repeat):
-        subject[RID][viscode][repeatcode] = [dict(new.iloc[i])]
+data_uniqueRID = data.RID.unique()
+subjectVariables = []
+visitVariables = []
+repeatVariables = []
+subject = {}
+for RID in data_uniqueRID:
+  subject[RID] = {}
+  row = data.loc[data['RID'] == RID]
+  visit = row["VISCODE"]
+  for viscode in visit:
+    if viscode not in subject[RID]:
+      subject[RID][viscode] = {}
+    vrow = row.loc[row['VISCODE'] == viscode]
+    repeat = vrow["REPEATCODE"]
+    new = vrow.drop(["RID", "VISCODE", "REPEATCODE"], axis = 1)
+    for i, repeatcode in enumerate(repeat):
+      subject[RID][viscode][repeatcode] = [dict(new.iloc[i])]
 
 """
   check subject level time stamp
@@ -56,26 +67,30 @@ with pd.read_csv("ClinicalInfo_final.csv") as data:
   these belong to the subject table
 """
 
+for column_name in subject[RID][viscode][repeatcode]:
+  colType = type(column_name)
+
 all_rid_same = {}
-for key in subject:
-  all_rid_same[key] = {}
-  for viscode in subject[key]:
-    for repeatcode in subject[key][viscode]:
-      for idx, _ in enumerate(subject[key][viscode][repeatcode]):
-        for column_name in subject[key][viscode][repeatcode][idx]:
-          entry_value = subject[key][viscode][repeatcode][idx][column_name]
-          if column_name not in all_rid_same[key]:
-            all_rid_same[key][column_name] = [entry_value]
+for RID in subject:
+  all_rid_same[RID] = {}
+  for viscode in subject[RID]:
+    for repeatcode in subject[RID][viscode]:
+      for idx, _ in enumerate(subject[RID][viscode][repeatcode]):
+        for column_name in subject[RID][viscode][repeatcode][idx]:
+          entry_value = subject[RID][viscode][repeatcode][idx][column_name]
+          print(type(column_name))
+          if column_name not in all_rid_same[RID]:
+            all_rid_same[RID][column_name] = [entry_value]
           else:
-            all_rid_same[key][column_name].append(entry_value)
+            all_rid_same[RID][column_name].append(entry_value)
 
 column_subject_eval = {}
 
-for key in all_rid_same:
-  for column_name in all_rid_same[key]:
+for RID in all_rid_same:
+  for column_name in all_rid_same[RID]:
     subject_bool = all(
-      value == all_rid_same[key][column_name][0]
-      for value in all_rid_same[key][column_name]
+      value == all_rid_same[RID][column_name][0]
+      for value in all_rid_same[RID][column_name]
     )
     if column_name not in column_subject_eval:
       column_subject_eval[column_name] = [subject_bool]
@@ -95,13 +110,13 @@ for column_name in column_subject_eval:
 column_visit_eval = {}
 
 
-for key in subject:
-  for viscode in subject[key]:
-    if (len(subject[key][viscode]) > 1):
-      for column_name in subject[key][viscode][repeatcode][0]:
+for RID in subject:
+  for viscode in subject[RID]:
+    if (len(subject[RID][viscode]) > 1):
+      for column_name in subject[RID][viscode][repeatcode][0]:
         store_values = []
-        for repeatcode in subject[key][viscode]:
-          for visit_dict in subject[key][viscode][repeatcode]:
+        for repeatcode in subject[RID][viscode]:
+          for visit_dict in subject[RID][viscode][repeatcode]:
             store_values.append(visit_dict[column_name])
             visit_bool = all(
             value == store_values[0]
@@ -118,12 +133,19 @@ for column_name in column_visit_eval:
   else:
     repeatVariables.append(column_name)
 
-with open("mockData.sql", "w+") as textfile:
-  
-  updateTableTemplate = open("createTableTemplate.sql").r
 
-  updateTableTemplate.format(tablename = "subject", col
-
-  updateTableTemplate.format(tablename = "visit", col
-  
-  updateTableTemplate.format(tablename = "repeat", columns)
+#with open(filename, "w+") as textfile:
+#  
+#  updateTableTemplate = "ALTER TABLE {tablename}\n"
+#
+#  updateTableTemplate.format(tablename = "subject")
+#
+#  for column in columns:
+#    updateTableTemplate = updateTableTemplate + "ADD {name} {datatype}".format(
+#      name = column.name,
+#      datatype = column.type,
+#    )
+#
+#  updateTableTemplate.format(tablename = "visit", col
+#
+#  updateTableTemplate.format(tablename = "repeat", columns)
