7b798a7da612efa4bccfbcd0024b341056b5a0fc
Paige T <ptuttosi@sfu.ca>
2020-01-10 22:42:05 -0800

    Add database connections

--- mockDatabase.py
+++ mockDatabase.py
@@ -16,22 +16,58 @@ for line in environment:
     creds[Username] = elements[1].strip()
   elif elements[0] == Password:
     creds[Password] = elements[1].strip()
+  elif elements[0] == Host:
+    creds[Host] = elements[1].strip()
+  elif elements[0] == Port:
+    creds[Port] = elements[1].strip()
+  elif elements[0] == Database:
+    creds[Database] = elements[1].strip()
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
-  "filename",
-  help="the name of the SQL file which will be created to hold ALTER statemnets"
-)
-args = parser.parse_args()
+connection = None
 
-with open(r"config.yaml") as config:
-  # The FullLoader parameter handles the conversion from YAML
-  # scalar values to Python the dictionary format
-  vars_list = yaml.load(config, Loader=yaml.FullLoader)
+try:
+    connection = psycopg2.connect(
+      user = creds[Username],
+      password = creds[Password],
+      host = creds[Host],
+      port = creds[Port],
+      database = creds[Database]
+    )
 
-#read in the data and save the headers to a list
-data = pd.read_csv("ClinicalInfo_final.csv")
+    cursor = connection.cursor()
+    # Print PostgreSQL Connection properties
+    print ( connection.get_dsn_parameters(),"\n")
 
-timePoint = findTimepoints(data)
+    # Print PostgreSQL version
+    cursor.execute("SELECT version();")
+    record = cursor.fetchone()
+    print("You are connected to - ", record,"\n")
 
-updateTables(args.filename, timePoint, vars_list)
+except (Exception, psycopg2.Error) as error :
+    print ("Error while connecting to PostgreSQL", error)
+    exit(0)
+finally:
+    #closing database connection.
+    if(connection):
+        cursor.close()
+        connection.close()
+        print("PostgreSQL connection is closed")
+
+#parser = argparse.ArgumentParser()
+#parser.add_argument(
+#  "filename",
+#  help="the name of the SQL file which will be created to hold ALTER statemnets"
+#)
+#args = parser.parse_args()
+#
+#with open(r"config.yaml") as config:
+#  # The FullLoader parameter handles the conversion from YAML
+#  # scalar values to Python the dictionary format
+#  vars_list = yaml.load(config, Loader=yaml.FullLoader)
+#
+##read in the data and save the headers to a list
+#data = pd.read_csv("ClinicalInfo_final.csv")
+#
+#timePoint = findTimepoints(data)
+#
+#updateTables(args.filename, timePoint, vars_list)
