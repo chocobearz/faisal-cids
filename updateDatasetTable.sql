a4e558a46b7fc2f8e679aaa554c4fa58a171efa2
Paige T <ptuttosi@sfu.ca>
2020-03-10 00:12:21 -0700

    add dataset info for ADNI

--- updateDatasetTable.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO mockschema.dataset (name, country, timeframe, affliliation, access)
-  VALUES ('mock', 'Canada', '2019-2020', 'Simon Fraser University', 'public');
---
+++ updateDatasetTableAlzheimer.sql
@@ -0,0 +1,2 @@
+INSERT INTO alzheimer.dataset (name, geographicregion, timeframe, participants, affliliation, access)
+  VALUES ('ADNI', 'North America', '2009-2016', 1821,'University of Southern California', 'Application');
