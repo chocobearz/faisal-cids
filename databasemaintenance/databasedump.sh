d104c470d98965c193b87e2cc0ac9e05b05f7c36
Paige T <ptuttosi@sfu.ca>
2020-05-10 14:30:40 -0700

    change paths for backups

--- databasemaintenance/databasedump.sh
+++ databasemaintenance/databasedump.sh
@@ -5,8 +5,6 @@ cd $path
 now=$(date +"%m_%d_%Y-%H.%M")
 pg_dump --username=ptuttosi --clean --port=5432 faisaldatabase > $path/backups/faisaldatabase_backup_$now.sql
 
-cd $path/backups
-
 #-- jobs=njobs  Run the dump in parallel by dumping njobs tables simultaneously.
 # This option reduces the time of the dump but it also increases the load on the
 # database server. You can only use this option with the directory output format
@@ -16,6 +14,6 @@ cd $path/backups
 #accommodate all connections. 
 #see https://www.postgresql.org/docs/9.3/app-pgdump.html
 
-md5sum faisaldatabase_backup_$now.sql > faisaldatabase_backup_$now.sql.md5sum
+md5sum faisaldatabase_backup_$now.sql > $path/backups/faisaldatabase_backup_$now.sql.md5sum
 
-$1faisal-databasetools/venv/bin/python checkmd5sum.py faisaldatabase_backup_$now.sql.md5sum
+$1faisal-databasetools/venv/bin/python checkmd5sum.py $path/backups/faisaldatabase_backup_$now.sql.md5sum
