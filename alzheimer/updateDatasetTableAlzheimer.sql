e7d33f451347a772a8cd08763bf8f43644da12c3
ptuttosi <ptuttosi@sfu.ca>
2020-08-17 18:35:53 -0700

    add datasetinfo for AIBL and PPMI

--- alzheimer/updateDatasetTableAlzheimer.sql
+++ alzheimer/updateDatasetTableAlzheimer.sql
@@ -1,2 +1,6 @@
 INSERT INTO alzheimer.dataset (name, geographicregion, timeframe, participants, affliliation, access)
   VALUES ('ADNI', 'North America', '2009-2016', 1821,'University of Southern California', 'Application');
+INSERT INTO alzheimer.dataset (name, geographicregion, timeframe, participants, affliliation, access)
+  VALUES ('AIBL', 'Oceania', '2006-2011', 1100,'CSIRO', 'Application');
+INSERT INTO alzheimer.dataset (name, geographicregion, timeframe, participants, affliliation, access)
+  VALUES ('PPMI', 'Various', '2010-2018', 170,'Michael J. Fox Foundation', 'Application');
