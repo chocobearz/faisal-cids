e0378fd7c15d8f87de4f031d75e88261b5897e4a
ptuttosi <ptuttosi@sfu.ca>
2020-08-22 20:27:58 -0700

    remove whitespce error

--- databasemaintenance/cronbackup.sh
+++ databasemaintenance/cronbackup.sh
@@ -1,4 +1,3 @@
 #database backup to run at 3 am every day (10 am UTC), will not write backup if no changes made to database 
 
 0 10 * * * /data/faisal-cids/databasemaintenance/databasedump.sh /data/
-
