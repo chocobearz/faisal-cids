b5baa626fb1b6fcb7c9a3ec87812dc0e93711773
ptuttosi <ptuttosi@sfu.ca>
2020-08-23 03:26:39 +0000

    update cron for root user

--- databasemaintenance/cronbackup.sh
+++ databasemaintenance/cronbackup.sh
@@ -1,3 +1,4 @@
 #database backup to run at 3 am every day (10 am UTC), will not write backup if no changes made to database 
 
-0 10 * * * /home/ptuttosi/faisalcids/faisal-cids/databasemaintenance/databasedump.sh
+0 10 * * * /data/faisal-cids/databasemaintenance/databasedump.sh /data/
+
