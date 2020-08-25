0b66546a0a8c06c2f959e1d78a7e89a24a626a42
ptuttosi <ptuttosi@sfu.ca>
2020-08-25 16:14:51 -0700

    add OASIS dataset info

--- alzheimer/updateDatasetTableAlzheimer.sql
+++ alzheimer/updateDatasetTableAlzheimer.sql
@@ -4,3 +4,7 @@ INSERT INTO alzheimer.dataset (name, geographicregion, timeframe, participants, 
   VALUES ('AIBL', 'Oceania', '2006-2011', 1100,'CSIRO', 'Application');
 INSERT INTO alzheimer.dataset (name, geographicregion, timeframe, participants, affliliation, access)
   VALUES ('PPMI', 'Various', '2010-2018', 170,'Michael J. Fox Foundation', 'Application');
+INSERT INTO alzheimer.dataset (name, geographicregion, timeframe, participants, affliliation, access)
+  VALUES ('OASIS_LONG', 'United States', '-2010', 150,'Washington University St.Louis', 'Open Access');
+INSERT INTO alzheimer.dataset (name, geographicregion, timeframe, participants, affliliation, access)
+  VALUES ('OASIS_CROSS', 'United States', '-2007', 416,'Washington University St.Louis', 'Open Access');
