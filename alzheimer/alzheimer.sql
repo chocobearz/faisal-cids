b1aeaa27c2f89a578a22c61dad0bd34a4a90da6d
chocobearz <ptuttosi@gmail.com>
2021-04-22 19:42:05 -0700

    add if not exist to sql setup

--- alzheimer/alzheimer.sql
+++ alzheimer/alzheimer.sql
@@ -8,10 +8,10 @@
 
 --as research intrests expand create new
 --Create schema
-CREATE SCHEMA alzheimer
+CREATE SCHEMA IF NOT EXISTS alzheimer;
 
 --holds info on the dataset
-CREATE TABLE alzheimer.dataset(
+CREATE TABLE IF NOT EXISTS alzheimer.dataset(
   id serial UNIQUE PRIMARY KEY,
   name varchar,
   geographicregion varchar,
@@ -22,19 +22,19 @@ CREATE TABLE alzheimer.dataset(
 );
 
 --holds info that remains constant over all visits
-CREATE TABLE alzheimer.subject(
+CREATE TABLE IF NOT EXISTS alzheimer.subject(
   id serial NOT NULL UNIQUE PRIMARY KEY,
   datasetid int NOT NULL REFERENCES alzheimer.dataset(id) ON DELETE CASCADE
 );
 
 --holds info that remains constant for the duration of a visit
-CREATE TABLE alzheimer.visit(
+CREATE TABLE IF NOT EXISTS alzheimer.visit(
   id serial NOT NULL UNIQUE PRIMARY KEY,
   subjectid int NOT NULL REFERENCES alzheimer.subject(id) ON DELETE CASCADE
 );
 
 --info that changes during the visit
-CREATE TABLE alzheimer.repeatmeasure(
+CREATE TABLE IF NOT EXISTS alzheimer.repeatmeasure(
   id serial NOT NULL UNIQUE PRIMARY KEY,
   visitid int NOT NULL REFERENCES alzheimer.visit(id) ON DELETE CASCADE
 );
